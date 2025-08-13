861dab087421c4b5c21144cf12562aec7f1a2353
alive4ever <alive4ever@users.noreply.github.com>
2025-08-13 08:53:42 +0000

    build: try downloading manually
    Use dpkg-source to extract the source after downloading.

--- build-songrec.sh
+++ build-songrec.sh
@@ -8,14 +8,11 @@ cd ~/build
 echo "deb-src [signed-by=/etc/apt/keyrings/songrec-ppa.asc] https://ppa.launchpadcontent.net/marin-m/songrec/ubuntu noble main" | sudo tee /etc/apt/sources.list.d/songrec.list
 curl -L "$SIGNING_KEY" | sudo tee /etc/apt/keyrings/songrec-ppa.asc
 sudo apt update
-apt source songrec
-mv songrec-${PKG_VERSION}${PPA_CODENAME} songrec-${PKG_VERSION}
-cd songrec-$PKG_VERSION
+apt source --download-only songrec
+dpkg-source -x songrec_${PKG_VERSION}${PPA_CODENAME}.dsc
+cd songrec-${PKG_VERSION}${PPA_CODENAME}
 sudo apt build-dep -y .
-dch -b --newversion $PKG_VERSION-1bpo2 --distribution trixie "Rebuild for trixie"
-cd ..
-dpkg-source -b songrec-${PKG_VERSION}
-cd songrec-$PKG_VERSION
+dch -b --newversion ${PKG_VERSION}${PPA_CODENAME}-1bpo0 --distribution trixie "Rebuild for trixie"
 debuild -us -uc
 cd ..
 mkdir -p /tmp/hosttmp/songrec_deb
